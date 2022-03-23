b192d31283d517c20a4139e9d00badcd3236076f
Ross <komarostyslav@gmail.com>
2022-03-23 18:18:09 +0200

    Lesson 10

--- women/models.py
+++ women/models.py
@@ -3,8 +3,8 @@ from django.urls import reverse
 
 
 class Women(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
+    title = models.CharField(max_length=255, verbose_name='Name Surname')
+    content = models.TextField(blank=True, verbose_name='Autobiography')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     time_created = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
@@ -17,12 +17,21 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = "Famous Women"
+        verbose_name_plural = "Famous Women"
+        ordering = ['time_created', 'title']
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='category')
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('cats', kwargs={'cat_id': self.pk})
+        return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
